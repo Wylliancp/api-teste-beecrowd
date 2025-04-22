a5deb0135d2d076d7d6276bb9eacf3f9e1a74c3e
“wylliancp” <wylliancp@gmail.com>
2025-04-21 23:26:14 -0300

    [bugfix] - adjust docker-compose

---
+++ src/Gateway/General.Gateway/Program.cs
@@ -0,0 +1,41 @@
+using Ocelot.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurar a configuração
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddJsonFile("ocelot.json")
+    .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+// Configurar serviços
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// builder.Services.AddOcelot(builder.Configuration)
+//     .AddTransientDefinedAggregator<BookDetailsAggregator>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
